2cf94e5b40b016f57f9b8bd28a905be4e4ae6a2d
Getachew <getchachewgeb333@gmail.com>
2020-03-13 08:36:48 -0400

    New Commit

---
+++ encapsulation-demo1/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		//creating instance of the encapsulated class  
+				Student0 s=new Student0("John","IT");  
+				//setting value in the name member  
+				//s.setName("vijay"); 
+				System.out.println(s.getName()+ " "+s.getDept()); 
+				Student0 s1=new Student0("Yeab","Business");
+				s1.setName("Getachew");
+				//getting value of the name member  
+				System.out.println(s1.getName() +" "+ s1.getDept());  
+				} 
+
+	}
+
